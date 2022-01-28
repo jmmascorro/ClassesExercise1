2da0183ae3032a99d935eecc40b8416f423ee2b7
jmmascorro <jessemascorro88@gmail.com>
2022-01-28 12:18:24 -0600

    finished Classes Exercise1

---
+++ Classes/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            var suv = new Car();
+            suv.Make = "Ford";
+            suv.Model = "Escape";
+            suv.Year = 2018;
+
+            var truck = new Car();
+            truck.Make = "Chevrolet";
+            truck.Model = "Silverado";
+            truck.Year = 2004;
+
+            var cars = new List<Car>();
+            cars.Add(truck);
+            cars.Add(suv);
+
+            Console.WriteLine("Make        Model      Year");
+            Console.WriteLine("-------------------------");
+            foreach (var car in cars)
+            {
+                
+                Console.WriteLine($"{car.Make}  {car.Model}  {car.Year}");
+            }
+            
+           
+
+
+
+
+
+        }
+    }
+}
